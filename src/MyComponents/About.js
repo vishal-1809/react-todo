b5d0a7bed0b83d3da56bd9b1ff7a4ea810e178f6
vishal-1809 <77793956+vishal-1809@users.noreply.github.com>
2021-07-08 20:57:46 +0530

    Add files via upload

---
+++ src/MyComponents/About.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            <p className="my-3">This is the react app created by Vishal</p>
+        </div>
+    )
+}
